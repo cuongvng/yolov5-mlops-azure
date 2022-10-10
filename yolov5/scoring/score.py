db68a253c32ef596d11056298269ad28cdc6abad
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2022-10-10 12:23:19 -0400

    deploy model to ACI

--- yolov5/scoring/score.py
+++ yolov5/scoring/score.py
@@ -23,68 +23,71 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE CODE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 """
-import numpy
-import joblib
 import os
+import torch
 from azureml.core.model import Model
-from inference_schema.schema_decorators \
-    import input_schema, output_schema
-from inference_schema.parameter_types.numpy_parameter_type \
-    import NumpyParameterType
+import subprocess
 
-
-def init():
-    # load the model from file into a global object
-    global model
-
-    # we assume that we have just one model
-    # AZUREML_MODEL_DIR is an environment variable created during deployment.
-    # It is the path to the model folder
-    # (./azureml-models/$MODEL_NAME/$VERSION)
-    model_path = Model.get_model_path(
-        os.getenv("AZUREML_MODEL_DIR").split('/')[-2])
-
-    model = joblib.load(model_path)
-
-
-input_sample = numpy.array([
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
-    [10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]])
-output_sample = numpy.array([
-    5021.509689995557,
-    3693.645386402646])
-
+# from inference_schema.schema_decorators \
+#     import input_schema, output_schema
+# from inference_schema.parameter_types.numpy_parameter_type \
+#     import NumpyParameterType
 
 # Inference_schema generates a schema for your web service
 # It then creates an OpenAPI (Swagger) specification for the web service
 # at http://<scoring_base_url>/swagger.json
-@input_schema('data', NumpyParameterType(input_sample))
-@output_schema(NumpyParameterType(output_sample))
-def run(data, request_headers):
-    result = model.predict(data)
+# @input_schema('data', NumpyParameterType(input_sample))
+# @output_schema(NumpyParameterType(output_sample))
 
-    # Demonstrate how we can log custom data into the Application Insights
-    # traces collection.
-    # The 'X-Ms-Request-id' value is generated internally and can be used to
-    # correlate a log entry with the Application Insights requests collection.
-    # The HTTP 'traceparent' header may be set by the caller to implement
-    # distributed tracing (per the W3C Trace Context proposed specification)
-    # and can be used to correlate the request to external systems.
-    print(('{{"RequestId":"{0}", '
-           '"TraceParent":"{1}", '
-           '"NumberOfPredictions":{2}}}'
-           ).format(
-               request_headers.get("X-Ms-Request-Id", ""),
-               request_headers.get("Traceparent", ""),
-               len(result)
-    ))
+YOLOV5_PATH = "./yolov5_repo"
+file_name = "zidane.jpg"
+subprocess.run(["pip", "install", "-r", YOLOV5_PATH + "/requirements.txt"])
 
-    return {"result": result.tolist()}
+def run(img_path, request_headers):
+
+    model_path = Model.get_model_path(
+        os.getenv("AZUREML_MODEL_DIR").split('/')[-2])
+    print("model_path", model_path)
+
+    prj_path = os.path.join(YOLOV5_PATH, "runs/detect")
+    
+    subprocess.run(["python", 
+                    os.path.join(YOLOV5_PATH, "detect.py"),
+                    "--weights", model_path,
+                    "--source", img_path,
+                    "--project", prj_path,
+                    "--img", 320
+                    ])
+
+    # Result is saved to `res_path/exp/file_name`
+    output_file = os.path.join(prj_path, "exp", file_name)
+    print("output_file", output_file)
+    subprocess.run(["cp", output_file, os.path.join("outputs", file_name)])
+
+    # Delete the output dir to avoid increment in later runs
+    subprocess.run(["rm", "-rf", os.path.join(prj_path, "exp")])
+
+
+    # # Demonstrate how we can log custom data into the Application Insights
+    # # traces collection.
+    # # The 'X-Ms-Request-id' value is generated internally and can be used to
+    # # correlate a log entry with the Application Insights requests collection.
+    # # The HTTP 'traceparent' header may be set by the caller to implement
+    # # distributed tracing (per the W3C Trace Context proposed specification)
+    # # and can be used to correlate the request to external systems.
+    # print(('{{"RequestId":"{0}", '
+    #        '"TraceParent":"{1}", '
+    #        '"NumberOfPredictions":{2}}}'
+    #        ).format(
+    #            request_headers.get("X-Ms-Request-Id", ""),
+    #            request_headers.get("Traceparent", ""),
+    #            len(result)
+    # ))
+
+    # return {"result": result.tolist()}
 
 
 if __name__ == "__main__":
-    # Test scoring
-    init()
-    test_row = '{"data":[[1,2,3,4,5,6,7,8,9,10],[10,9,8,7,6,5,4,3,2,1]]}'
-    prediction = run(test_row, {})
-    print("Test result: ", prediction)
+    test_img = os.path.join("./scoring", file_name)
+    subprocess.run(["wget", "https://raw.githubusercontent.com/cuongvng/yolov5/master/data/images/zidane.jpg", test_img])
+    run(test_img, {})
