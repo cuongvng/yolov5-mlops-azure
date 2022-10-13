1b1eb32da08c4030d907801423bb67cbbdd8f842
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2022-10-13 16:23:18 -0400

    move .env file into yolov5 working dir

--- ml_service/util/env_variables.py
+++ ml_service/util/env_variables.py
@@ -13,7 +13,7 @@ class Env:
     """
 
     # to load .env file into environment variables for local execution
-    load_dotenv()
+    load_dotenv("./yolov5/.env")
     workspace_name: Optional[str] = os.environ.get("WORKSPACE_NAME")
     resource_group: Optional[str] = os.environ.get("RESOURCE_GROUP")
     subscription_id: Optional[str] = os.environ.get("SUBSCRIPTION_ID")
